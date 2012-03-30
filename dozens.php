7d1f5bcb2d6ddb6cc4a9390f78863e26522659d8
DOZEN <kyosuke6428@gmail.com>
2012-03-31 04:41:41 +0900

    コメントを追加

--- dozens.php
+++ dozens.php
@@ -12,6 +12,24 @@ class Config {
   const MEMCACHEDHOST = 'localhost';
   const MEMCACHEDPORT = 11211;
 
+  //以上で設定終了
+
+  /*
+   * 更新対象となるドメイン群を読み込んで配列の形で返す。
+   * ↓こんなかんじ [0]はゾーン名、[1]はドメイン名
+   * (
+   * [0] => Array
+   *     (
+   *         [0] => hoge.jp
+   *         [1] => www.hoge.jp
+   *     )
+   * [1] => Array
+   *     (
+   *         [0] => foo.jp
+   *         [1] => www.foo.jp
+   *     )
+   * )
+   */
   static function targetList() {
     $targetList = array();
     $file = file(__DIR__ . 'targetlist');
@@ -29,10 +47,12 @@ class Config {
 
 class MyIPAddress {
 
+  //現在のIPアドレスを取得
   function __construct() {
     $this->ip = trim(file_get_contents('http://ifconfig.me/ip'));
   }
 
+  //現在のIPアドレスを返す
   function getMyIPAddress() {
     return $this->ip;
   }
@@ -47,13 +67,16 @@ class MyIPAddress {
       }
       return true;
     }
-    $file = file(__DIR__ . 'myipaddress');
-    if (trim($file[0]) == $this->ip) {
-      return false;
+    if (file_exists(__DIR__ . 'myipaddress')) {
+      $file = file(__DIR__ . 'myipaddress');
+      if (trim($file[0]) == $this->ip) {
+        return false;
+      }
     }
     return true;
   }
 
+  //次回実行時にIPアドレスの変動をチェックするため、IPアドレスを保存する。
   function setChanged() {
     if (Config::USEMEMCACHED) {
       $this->mem->set('dozens_MyIPAddress', $this->ip, 0, 3600);
@@ -70,6 +93,7 @@ class AuthRequest {
 
   private $recordList;
 
+  //XAUTHトークンを取得
   function __construct() {
     $this->curl = new Curl();
     $this->curl->create('authentication');
@@ -77,8 +101,8 @@ class AuthRequest {
     $this->auth_token = $this->curl->result()->auth_token;
   }
 
+  //レコードリストを取得
   function getRecordList($zoneName) {
-
     if (!isset($this->recordList[$zoneName])) {
       $this->curl->create('getrecordlist');
       $this->curl->setOptions('http://dozens.jp/api/record/' . $zoneName . '.json', array('auth_token' => $this->auth_token));
@@ -87,6 +111,7 @@ class AuthRequest {
     return $this->recordList[$zoneName];
   }
 
+  //レコードを更新
   function updateRecord($recordID, $options) {
     $this->curl->create('updaterecord');
     $this->curl->setOptions('http://dozens.jp/api/record/update/' . $recordID . '.json', array('auth_token' => $this->auth_token, 'json' => json_encode($options)));
@@ -120,6 +145,7 @@ class Curl {
           'X-Auth-Token: ' . $options['auth_token'],
           'Content-Type: application/json'
       );
+      //レコードの更新の時はPOSTにする
       if ($this->type == 'updaterecord') {
         $curlOptions += array(
             CURLOPT_POST => 1,
@@ -130,6 +156,7 @@ class Curl {
     curl_setopt_array($this->ch, $curlOptions);
   }
 
+  //cURLの実行結果を返す
   public function result() {
     $result = json_decode(curl_exec($this->ch));
     curl_close($this->ch);
@@ -144,6 +171,7 @@ class Dozens {
     $this->auth = new AuthRequest();
   }
 
+  //レコードの更新に必要なレコードのID, prio, ttlを返す。
   function getRecordListofTarget($targetList) {
     $recordList = array();
     $updateList = array();
@@ -163,6 +191,7 @@ class Dozens {
     return $updateList;
   }
 
+  //レコードを更新する
   function updateRecords($updateList) {
     foreach ($updateList as $line) {
       $this->auth->updateRecord($line['ID'], array(
@@ -173,6 +202,7 @@ class Dozens {
     }
   }
 
+  //現在のIPアドレスをセットする
   function setMyIPAddress($ip) {
     $this->ip = $ip;
   }
